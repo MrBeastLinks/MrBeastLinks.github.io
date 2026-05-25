59474680acc2dd511986cedb0eef35c75cfdbe9d
CGTutorial <christiangoblininfo@gmail.com>
2026-05-25 19:02:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+body {
+    background: #111;
+    color: white;
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 40px;
+}
+
+header {
+    text-align: center;
+    margin-bottom: 50px;
+}
+
+button {
+    padding: 10px 20px;
+    border: none;
+    background: #00ff88;
+    color: black;
+    cursor: pointer;
+}
 
